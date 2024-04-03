bc9bcc058fc3c11a8dc2461e6c125f2cab1db113
Heisuzul <heidy.mina@correounivalle.edu.co>
2024-04-03 18:08:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const typeDefs = gql`
   type Query {
     hello(message: String!): String
+    aboutHeidy(): String
   }
 `;
 
@@ -15,6 +16,9 @@ const resolvers = {
     hello: (_, { message }) => {
         return `¡Hola, ${message}! Un saludo por parte del profe `;
       },
+      aboutHeidy: () => {
+        return 'Me gusta ir al gimnasio'
+      }
   },
 };
 
